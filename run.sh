bfd96a423c95aa55982dfe7911b24a63d8a79cf0
VlexandreMG <dodolashmou@gmail.com>
2026-07-30 01:56:25 +0300

    Debug kely

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ *.cpp -o mon_jeu -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+g++ src/*.cpp src/entities/*.cpp -o src/mon_jeu -I src -I src/entities -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 src/mon_jeu
--- src/entities/Ball.cpp
+++ src/entities/Ball.cpp
@@ -1,13 +1,14 @@
 #include "Ball.hpp"
 
-Ball::Ball(int startX, int startY, float startSpeedX , float startSpeedY , int startRadius );
+Ball::Ball(int startX, int startY, float startSpeedX , float startSpeedY , int startRadius ) {
     x = startX;
     y = startY;
-    SpeedX = startSpeedX;
-    SpeedY = startSpeedY;
+    speedX = startSpeedX;
+    speedY = startSpeedY;
     radius = startRadius;
+}
 
-void update() {
-    x += (int)SpeedX;
-    y += (int)SpeedY;
+void Ball::update() {
+    x += (int)speedX;
+    y += (int)speedY;
 }
