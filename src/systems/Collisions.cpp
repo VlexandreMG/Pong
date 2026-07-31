4a7df32e39198b3455a79f8924d187856f75fcfb
VlexandreMG <dodolashmou@gmail.com>
2026-07-31 11:20:16 +0300

    Mande le izy

--- src/systems/Collisions.cpp
+++ src/systems/Collisions.cpp
@@ -2,11 +2,11 @@
 #include "../entities/Ball.hpp"
 
 void Collisions::detectCollision(Ball& thisBall, int screenHeight , int screenWidth) {
-    if (thisBall.y + thisBall.radius >= screenHeight) {
+    if (thisBall.y + thisBall.radius >= screenHeight || thisBall.y - thisBall.radius <= 0) {
         thisBall.speedY = -(thisBall.speedY);
     } 
     
-    if (thisBall.x + thisBall.radius >= screenWidth) {
+    if (thisBall.x + thisBall.radius >= screenWidth || thisBall.x - thisBall.radius <= 0) {
         thisBall.speedX = -(thisBall.speedX);
     }
 }
