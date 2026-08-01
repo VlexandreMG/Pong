4fe6189cd6180786e339c40b14720bbb10dc0a92
VlexandreMG <dodolashmou@gmail.com>
2026-08-01 15:35:55 +0300

    Mandede le line

--- src/Window.cpp
+++ src/Window.cpp
@@ -16,11 +16,21 @@ int main() {
     Ball ball(startX,startY,5.0f,5.0f,20);
     Collisions collision;
     Line net = {{200.0f,80.0f},{200.0f, 600.0f}, 100.0f, LIGHTGRAY};
+    float lineSpeed = 5.0f;
 
     // Chargement des ressources (textures, sons...) une seule fois ici
 
     // === BOUCLE PRINCIPALE ===
     while (!WindowShouldClose()) {   // détecte ESC ou clic sur la croix par défaut
+
+        if (IsKeyDown(KEY_UP)) {
+            net.moveY(-lineSpeed);
+        }
+
+
+        if (IsKeyDown(KEY_DOWN)) {
+            net.moveY(lineSpeed);
+        }
 
         ball.update();
         collision.detectCollision(ball,screenHeight,screenWidth);
