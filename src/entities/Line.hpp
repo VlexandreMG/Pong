559fd91d569b60271189adcb1caf85f4d22dd1e7
VlexandreMG <dodolashmou@gmail.com>
2026-08-05 19:46:24 +0300

    startPos oui

--- src/entities/Line.hpp
+++ src/entities/Line.hpp
@@ -10,7 +10,15 @@ public:
     float speed;
 
     void moveY(float amount) {
+        
+        float lineLength = endPos.y - startPos.y;
+        
         startPos.y += amount;
         endPos.y += amount;
+
+        if (startPos.y < 0.0f) {
+            startPos.y = 0.0f;
+            endPos.y = startPos.y + lineLength;
+        }
     }
 };
